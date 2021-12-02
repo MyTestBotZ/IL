77f853b32ec706554cf7809199de84061df03718
Esther-Lopez <66007532+Esther-Lopez@users.noreply.github.com>
2021-12-02 14:53:52 +0530

    Update vars.py

--- Code_X_Mania/vars.py
+++ Code_X_Mania/vars.py
@@ -10,14 +10,14 @@ class Var(object):
     API_ID = int(getenv('API_ID'))
     API_HASH = str(getenv('API_HASH'))
     BOT_TOKEN = str(getenv('BOT_TOKEN'))
-    SESSION_NAME = str(getenv('SESSION_NAME', 'F2LxBot'))
+    SESSION_NAME = str(getenv('SESSION_NAME', 'tginstantlink'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
     BIN_CHANNEL = int(getenv('BIN_CHANNEL'))
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     #OWNER_ID = int(getenv('OWNER_ID', '2068472689'))
-    OWNER_ID = "2068472689"
+    OWNER_ID = "1248974748"
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
     OWNER_USERNAME = str(getenv('OWNER_USERNAME'))
